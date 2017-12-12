f6ff9e193dd1bd23fe3a9ce6d2287b0455e270fc
杨胜富 <ysfnevercry@gmail.com>
2017-12-12 09:10:43 +0800

    add login view

---
+++ Pangolin/Controllers/PALoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  PALoginViewController.h
+//  Pangolin
+//
+//  Created by yangshengfu on 11/12/2017.
+//  Copyright © 2017 Minivision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PALoginViewController : UIViewController
+- (IBAction)userNameTextField:(UITextField *)sender;
+- (IBAction)passwordTextField:(UITextField *)sender;
+- (IBAction)LoginButton:(UIButton *)sender;
+- (IBAction)registerButton:(UIButton *)sender;
+
+@end
---
+++ Pangolin/Controllers/PALoginViewController.m
@@ -0,0 +1,48 @@
+//
+//  PALoginViewController.m
+//  Pangolin
+//
+//  Created by yangshengfu on 11/12/2017.
+//  Copyright © 2017 Minivision. All rights reserved.
+//
+
+#import "PALoginViewController.h"
+
+@interface PALoginViewController ()
+
+@end
+
+@implementation PALoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)userNameTextField:(UITextField *)sender {
+}
+
+- (IBAction)passwordTextField:(UITextField *)sender {
+}
+
+- (IBAction)LoginButton:(UIButton *)sender {
+}
+
+- (IBAction)registerButton:(UIButton *)sender {
+}
+@end
